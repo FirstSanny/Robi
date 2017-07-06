dbe78531fb3e5a1260f310047dad2a69cd04b6f4
ale.bartsch <ale.bartsch@mailbox.tu-berlin.de>
2017-07-06 11:57:57 +0200

    some more beaty

--- src/robi.py
+++ src/robi.py
@@ -1,6 +1,7 @@
 '''
 This is the main-file for our RoboCup project
-The Robo should search for 2 objects and bring it to a special destination for each object
+The Robo should search for 2 objects and bring it to
+a special destination for each object
 '''
 
 from naoqi import ALProxy
@@ -11,14 +12,15 @@ from optparse import OptionParser
 DEFAULT_NAO_IP = "nao.local"
 DEFAULT_NAO_PORT = 9559
 
+
 def main(pip, pport):
     '''
     ENTRY-POINT
     '''
-    
-    # Init Proxy's    
+
+    # Init Proxy's
     ttsProxy = ALProxy("ALTextToSpeech", pip, pport)
-    motionProxy  = ALProxy("ALMotion", pip, pport)
+    motionProxy = ALProxy("ALMotion", pip, pport)
     postureProxy = ALProxy("ALRobotPosture", pip, pport)
 
     # Wake up robot
@@ -27,9 +29,9 @@ def main(pip, pport):
     # Send robot to Stand Init
     postureProxy.goToPosture("StandInit", 0.5)
     ttsProxy.say("Hello Friends.")
-    
+
     ttsProxy.say("I am getting on my way.")
-    
+
 
 if __name__ == "__main__":
     # Parsing Commandlineparams
@@ -44,9 +46,9 @@ if __name__ == "__main__":
     parser.set_defaults(
         pip=DEFAULT_NAO_IP,
         pport=DEFAULT_NAO_PORT)
-    
+
     (opts, args_) = parser.parse_args()
-    pip   = opts.pip
+    pip = opts.pip
     pport = opts.pport
-    
+
     main(pip, pport)
