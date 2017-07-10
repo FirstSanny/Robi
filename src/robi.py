10b03be75de0956f6e0bfbf73aefb9cb52c9da36
Alessandro Bartsch <alebartsch@hotmail.com>
2017-07-10 19:50:02 +0200

    tried to move in the direction

--- src/robi.py
+++ src/robi.py
@@ -32,6 +32,55 @@ orange_up = np.array([15, 255, 255])
 
 orangedetected = False
 
+
+def searchForTheBall(motionProxy, visionProxy):
+    global orangedetected
+    orangedetected = False
+    
+    while not(orangedetected):   
+        cameraId = 0
+        
+        data = visionProxy.getBGR24Image(cameraId)
+        image = np.fromstring(data, dtype=np.uint8).reshape((480, 640, 3)) #cv2.imshow('image', image)
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        
+        mask = cv2.inRange(hsv_img, orange_down, orange_up)
+        moments = cv2.moments(mask)
+        area = moments['m00']
+       
+        #mask2 = cv2.inRange(image, orange_down, orange_up)
+        #moments = cv2.moments(mask2)
+        #area = moments['m00']
+       
+        cv2.imshow('hsv_img', hsv_img)
+        cv2.imshow('mask', mask)
+        #cv2.imshow('mask_image', mask2)
+        
+        if (area > 20000):
+            # Searching for center (x,y) from object
+            x = int(moments['m10'] / moments['m00'])
+            y = int(moments['m01'] / moments['m00'])
+            
+            # print coordinates on console
+            print "x = ", x
+            print "y = ", y
+            
+            # mark the object with an red circle
+            rect_image = cv2.rectangle(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), (x - 50, y - 50), (x + 50, y + 50), (0, 0, 255), 2)
+            cv2.imshow('rec_image', rect_image)
+            
+            orangedetected = True
+        while 1:
+            key = cv2.waitKey(5) & 0xFF
+            if key == 27:
+                break
+        
+        if not orangedetected:
+            motionProxy.moveTo(0, 0, PI / 16)
+        
+        #TODO: Compute Distance and return it
+        return 0.3
+
 def main():
     '''
     ENTRY-POINT
@@ -45,8 +94,6 @@ def main():
     # Wake up robot
     motionProxy.setStiffnesses("Body", 1.0)
     motionProxy.wakeUp()
-    global orangedetected
-
 
     # Send robot to Stand Init
     motionProxy.moveInit()
@@ -55,56 +102,20 @@ def main():
     #getVisionResults(0)
 
     ttsProxy.say("Going to search for the ball.")
-    while not(orangedetected):
-        cameraId = 0
-        data = visionProxy.getBGR24Image(cameraId)
-        image = np.fromstring(data, dtype=np.uint8).reshape((480, 640, 3))
-        #cv2.imshow('image', image)
-        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        mask = cv2.inRange(hsv_img, orange_down, orange_up)
-        moments = cv2.moments(mask)
-        area = moments['m00']
-
-        #mask2 = cv2.inRange(image, orange_down, orange_up)
-        #moments = cv2.moments(mask2)
-        #area = moments['m00']
-
-        cv2.imshow('hsv_img', hsv_img)
-        cv2.imshow('mask', mask)
-        #cv2.imshow('mask_image', mask2)
-
-        if (area > 20000):
-            # Searching for center (x,y) from object
-            x = int(moments['m10'] / moments['m00'])
-            y = int(moments['m01'] / moments['m00'])
-
-            # print coordinates on console
-            print "x = ", x
-            print "y = ", y
-
-            # mark the object with an red circle
-            rect_image = cv2.rectangle(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), (x - 50, y - 50), (x + 50, y + 50), (0, 0, 255), 2)
-            cv2.imshow('rec_image', rect_image)
-
-            orangedetected = True
-
+    distance = 100
+    
+    while(distance > 0.2):
+        distance = searchForTheBall(motionProxy, visionProxy)
+        
         while(1):
             key = cv2.waitKey(5) & 0xFF
             if key == 27:
                 break
-
-
-        if not orangedetected:
-            motionProxy.moveTo(0, 0, PI / 16)
-    while(1):
-        key = cv2.waitKey(5) & 0xFF
-        if key == 27:
-            break
-    cv2.destroyAllWindows()
-
-
-    ttsProxy.say("Found the Ball.")
+        cv2.destroyAllWindows()
+    
+        ttsProxy.say("Found the Ball. Moving toward it.")
+        motionProxy.move(distance/8, 0, 0);
+    
 
 
 if __name__ == "__main__":
