78994998d0c446f74bab966e9d9491554328146f
rebeca <fernandezniederacher@campus.tu-berlin.de>
2017-07-11 16:38:05 +0200

    robot walks to the object and working on recognize_posture

---
+++ src/recognize_posture.py
@@ -0,0 +1,60 @@
+'''In this exercise you need to use the learned classifier to recognize current posture of robot
+* Tasks:
+    1. load learned classifier in `PostureRecognitionAgent.__init__`
+    2. recognize current posture in `PostureRecognitionAgent.recognize_posture`
+* Hints:
+    Let the robot execute different keyframes, and recognize these postures.
+'''
+
+import pickle
+from angle_interpolation import AngleInterpolationAgent
+from keyframes import hello, leftBackToStand, leftBellyToStand, rightBackToStand, rightBellyToStand, wipe_forehead
+
+import numpy
+from os import listdir, path
+
+ROBOT_POSE_CLF = 'robot_pose.pkl'
+ROBOT_POSE_DATA_DIR = 'robot_pose_data'
+classes = listdir(ROBOT_POSE_DATA_DIR)
+
+
+class PostureRecognitionAgent(AngleInterpolationAgent):
+    def __init__(self, simspark_ip='localhost',
+                 simspark_port=3100,
+                 teamname='DAInamite',
+                 player_id=0,
+                 sync_mode=True):
+        super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        self.posture = 'unknown'
+        self.posture_classifier = pickle.load(open(ROBOT_POSE_CLF))  # LOAD YOUR CLASSIFIER
+
+    def think(self, perception):
+        self.posture = self.recognize_posture(perception)
+        return super(PostureRecognitionAgent, self).think(perception)
+
+    def recognize_posture(self, perception):
+        posture = 'unknown'
+        # YOUR CODE HERE
+        data = []
+        data.append(perception.joint['LHipYawPitch'])
+        data.append(perception.joint['LHipRoll'])
+        data.append(perception.joint['LHipPitch'])
+        data.append(perception.joint['LKneePitch'])
+        data.append(perception.joint['RHipYawPitch'])
+        data.append(perception.joint['RHipRoll'])
+        data.append(perception.joint['RHipPitch'])
+        data.append(perception.joint['RKneePitch'])
+        data += perception.imu
+
+        data = numpy.array(data).reshape(1, -1)
+
+        index = self.posture_classifier.predict(data)
+        posture = classes[index[0]]
+        return posture
+
+
+if __name__ == '__main__':
+    agent = PostureRecognitionAgent()
+    #agent.keyframes = leftBackToStand()  # CHANGE DIFFERENT
+    agent.keyframes = rightBackToStand()
+    agent.run()
--- src/robi.py
+++ src/robi.py
@@ -7,32 +7,31 @@ from math import pi as PI
 from naoqi import ALProxy
 from optparse import OptionParser
 import numpy as np
+from os import listdir, path
+import pickle
 import cv2
 import math
 
 # CONSTANTS
 #DEFAULT_NAO_IP = "nao3.local"
-DEFAULT_NAO_IP = "10.0.7.13"
+DEFAULT_NAO_IP = "10.0.7.16"
 DEFAULT_NAO_PORT = 9559
+
+ROBOT_POSE_CLF = 'recources/robot_pose.pkl'
+ROBOT_POSE_DATA_DIR = 'recources/robot_pose_data'
+classes = listdir(ROBOT_POSE_DATA_DIR)
 
 #global variables - ports
 pip = None
 pport = None
 #global variables - constants
 sizeOfObject = 210
-focus = 30
-cameraId = 0
-
-###RGB##
-#orange_down = np.array([16, 48, 91])
-#orange_up = np.array([46, 100, 100])
-
-###BGR###
-#orange_down = np.array([153, 204, 255])
-#orange_up = np.array([0, 102, 102])
+focus = 300
+cameraId = 1
+cte_size = 1190
 
 ###HSV###
-orange_down = np.array([5, 50, 50])
+orange_down = np.array([10, 150, 150])
 orange_up = np.array([15, 255, 255])
 
 #global variables
@@ -61,11 +60,11 @@ def searchForTheBall(motionProxy, visionProxy):
         #moments = cv2.moments(mask2)
         #area = moments['m00']
 
-        cv2.imshow('hsv_img', hsv_img)
+        #cv2.imshow('hsv_img', hsv_img)
         cv2.imshow('mask', mask)
         cv2.imshow('image', image)
 
-        if (area > 20000000):
+        if (area > 500000):
             # Searching for center (x,y) from object
             x = int(moments['m10'] / moments['m00'])
             y = int(moments['m01'] / moments['m00'])
@@ -77,7 +76,8 @@ def searchForTheBall(motionProxy, visionProxy):
             print "computedSize = ", computedSize
 
             # calculate distance
-            dist = (sizeOfObject / computedSize) * focus
+            #dist = (sizeOfObject / computedSize) * focus
+            dist = cte_size / computedSize
             print "distance = ", dist
 
             # mark the object with an red circle
@@ -86,8 +86,8 @@ def searchForTheBall(motionProxy, visionProxy):
             (x , y ), (x + 50, y + 50), (0, 0, 255), 2)
             cv2.imshow('rec_image', rect_image)
 
-            draw_image = cv2.drawContours(rgb_img, [mask], 1, (0, 255, 0), 2)
-            cv2.imshow('draw_image', draw_image)
+           # draw_image = cv2.drawContours(rgb_img, [mask], 1, (0, 255, 0), 2)
+            #cv2.imshow('draw_image', draw_image)
 
             orangedetected = True
 
@@ -117,6 +117,26 @@ def makeFotoOfOrangeQuadrat(visionProxy):
             break
     cv2.imwrite(orange_object, orange_image)
 
+def recognize_posture(motionProxy):
+    posture_classifier = pickle.load(open(ROBOT_POSE_CLF))
+    posture = 'unknown'
+    perception = motionProxy.getPerception()
+    data = {}
+    data.append(perception.joint['LHipYawPitch'])
+    data.append(perception.joint['LHipRoll'])
+    data.append(perception.joint['LHipPitch'])
+    data.append(perception.joint['LKneePitch'])
+    data.append(perception.joint['RHipYawPitch'])
+    data.append(perception.joint['RHipRoll'])
+    data.append(perception.joint['RHipPitch'])
+    data.append(perception.joint['RKneePitch'])
+    data += perception.imu
+
+    data = numpy.array(data).reshape(1, -1)
+
+    index = self.posture_classifier.predict(data)
+    posture = classes[index[0]]
+    return posture
 
 def main():
     '''
@@ -149,7 +169,8 @@ def main():
         cv2.destroyAllWindows()
 
         ttsProxy.say("Found the Ball. Moving toward it.")
-        motionProxy.move(distance - 0.1, 0, 0)
+        print "distance to walk =", distance/2
+        motionProxy.moveTo(distance / 2 , 0, 0)
     ttsProxy.say("In front of the ball")
     makeFotoOfOrangeQuadrat(visionProxy)
 
