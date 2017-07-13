f854efb63787b097d28b14b5560516f640528c19
rebeca <fernandezniederacher@campus.tu-berlin.de>
2017-07-13 17:46:59 +0200

    working

--- src/robi.py
+++ src/robi.py
@@ -14,7 +14,7 @@ import math
 
 #** Connection **#
 #DEFAULT_NAO_IP = "nao3.local"
-DEFAULT_NAO_IP = "10.0.7.16"
+DEFAULT_NAO_IP = "10.0.7.13"
 DEFAULT_NAO_PORT = 9559
 
 #** sizes **#
@@ -29,8 +29,8 @@ FELL_DOWN = -1
 ORANGE_DOWN = np.array([10, 150, 150])
 ORANGE_UP = np.array([15, 255, 255])
 
-PINK_DOWN = np.array([300, 150, 150])
-PINK_UP = np.array([320, 255, 255])
+PINK_DOWN = np.array([150, 100, 100])
+PINK_UP = np.array([200, 255, 255])
 
 ### GLOBAL VARIABLES ###
 
@@ -51,36 +51,47 @@ def getPictureFromOneCamera(colorDown, colorUp, cameraId):
         mask = cv2.inRange(hsv_img, colorDown, colorUp)
         moments = cv2.moments(mask)
         area = moments['m00']
-        return area, image, moments
+        return area, image, moments, mask
 
 
 def getBetterPicture(colorDown, colorUp):
-        area0, image0, moments0 = getPictureFromOneCamera(visionProxy,
-        colorDown, colorUp, CAM_ID_TOP)
+        area0, image0, moments0, mask0 = getPictureFromOneCamera(colorDown, colorUp, CAM_ID_TOP)
 
-        area1, image1, moments1 = getPictureFromOneCamera(visionProxy,
-        colorDown, colorUp, CAM_ID_BOTTOM)
+        area1, image1, moments1, mask1 = getPictureFromOneCamera(colorDown, colorUp, CAM_ID_BOTTOM)
 
         if area0 > area1:
-            return area0, image0, moments0
+            return area0, image0, moments0, mask0
         else:
-            return area1, image1, moments1
+            return area1, image1, moments1, mask1
 
 
 def searchForColor(colorDown, colorUp):
     detected = False
     dist = 0
-    #Getting the picture with the bigger area of the color
-    area, image, moments = getBetterPicture(visionProxy, colorDown, colorUp)
 
     while not(detected):
 
-        cv2.imshow('image', image)
+        # Getting the picture with the bigger area of the color
+        area, image, moments, mask = getBetterPicture(colorDown, colorUp)
+
+        #cv2.imshow('image', image)
+        #cv2.imshow('mask', mask)
+        #while 1:
+        #    key = cv2.waitKey(5) & 0xFF
+        #    if key == 27:
+        #        break
+        #cv2.destroyAllWindows()
 
         if (area > 500000):
             # Searching for center (x,y) from object
             x = int(moments['m10'] / moments['m00'])
             y = int(moments['m01'] / moments['m00'])
+
+            if x < 150 or x > 450:
+                if (not moveTo(0, 0, PI / 16)):
+                    return FELL_DOWN
+                continue
+
             computedSize = math.sqrt(area)
 
             # calculate distance
@@ -92,89 +103,97 @@ def searchForColor(colorDown, colorUp):
             #rect_image = cv2.rectangle(rgb_img,
             #(x, y), (x + 50, y + 50), (0, 0, 255), 2)
             #cv2.imshow('rec_image', rect_image)
-            cir_image = cv2.circle(rgb_img, (x, y), 50, Scalar(255, 0, 0), 3)
+            cir_image = cv2.circle(rgb_img, (x, y), 50, (255, 0, 0), 3)
             cv2.imshow('circ_image', cir_image)
 
             detected = True
 
-        while 1:
-            key = cv2.waitKey(5) & 0xFF
-            if key == 27:
-                break
         #turn to search in another direction
         if not detected:
-            if(not moveTo(motionProxy, ttsProxy, 0, 0, PI / 16)):
+            if(not moveTo(0, 0, PI / 16)):
                 return FELL_DOWN
 
     return dist
 
 
-def makeFotoOfQuadrat(imgTitle):
+def makeFotoOfQuadrat(imgTitle, colorDown, colorUp):
     # find quadrat
-    data = visionProxy.getBGR24Image(CAM_ID_BOTTOM)
-    image = np.fromstring(data,
-    dtype=np.uint8).reshape((480, 640, 3))
+    area, image, moments, mask = getBetterPicture(colorDown, colorUp)
 
     #show and write
-    cv2.imshow('image', image)
-    while 1:
-        key = cv2.waitKey(5) & 0xFF
-        if key == 27:
-            break
-    cv2.imwrite(imgTitle, image)
+    #cv2.imshow('image', image)
+    #while 1:
+    #    key = cv2.waitKey(5) & 0xFF
+    #    if key == 27:
+    #        break
+    #cv2.destroyAllWindows()
+    x = int(moments['m10'] / moments['m00'])
+    y = int(moments['m01'] / moments['m00'])
+
+    cir_image = cv2.circle(image, (x, y), 50, (255, 0, 0), 3)
+    cv2.imwrite(imgTitle, cir_image)
 
 
 def searchColorAndTakeFoto(imgTitle, colorDown, colorUp):
     distance = FELL_DOWN
 
-    while(distance > 0.2 or distance == FELL_DOWN):
+    while(distance > 0.3 or distance == FELL_DOWN):
         if(distance == FELL_DOWN):
-            initMotion()
+            initMotion(True)
         distance = searchForColor(colorDown, colorUp)
-        if(distance == FELL_DOWN):
+        if(distance == FELL_DOWN or distance <= 0.3):
             continue
 
-        while(1):
-            key = cv2.waitKey(5) & 0xFF
-            if key == 27:
-                break
-        cv2.destroyAllWindows()
+        #while(1):
+        #    key = cv2.waitKey(5) & 0xFF
+        #    if key == 27:
+        #        break
+        #cv2.destroyAllWindows()
 
         ttsProxy.say("Found the square object. moving to it.")
         distToWalk = distance / 2
         print "distance to walk =", distToWalk
         if(not moveTo(distToWalk, 0, 0)):
             distance = FELL_DOWN
+        distance = distToWalk
 
     ttsProxy.say("I'm near enough to make the foto")
-    makeFotoOfQuadrat(imgTitle)
+    makeFotoOfQuadrat(imgTitle, colorDown, colorUp)
 
 
 def setHeadCentral():
     yawAngle = motionProxy.getAngles("HeadYaw", True)
     pitchAngle = motionProxy.getAngles("HeadPitch", True)
-    motionProxy.changeHeadAngles(-1 * yawAngle, -1 * pitchAngle, 0.1)
+    motionProxy.changeHeadAngles(-1 * yawAngle[0], -1 * pitchAngle[0], 0.1)
 
 
 def moveTo(x, y, theta):
-    motionProxy.post.moveTo(x, y, theta)
-    while(motionProxy.moveIsActive()):
-        if(motionProxy.getPosture() == "Back" or
-        motionProxy.getPosture() == "Belly"):
-            ttsProxy.say("Damnit. Can anybody help me standing up?")
-            return False
+    motionProxy.moveTo(x, y, theta)
+    #while(motionProxy.post.moveIsActive()):
+    if(motionProxy.getPosture() == "Back" or
+    motionProxy.getPosture() == "Belly"):
+        ttsProxy.say("Damnit. Can anybody help me standing up?")
+        return False
+    motionProxy.waitUntilWalkIsFinished()
+    if(motionProxy.getPosture() == "Back" or
+    motionProxy.getPosture() == "Belly"):
+        ttsProxy.say("Damnit. Can anybody help me standing up?")
+        return False
     return True
 
 
-def initMotion():
+def initMotion(shouldRest):
     # Wake up robot
     motionProxy.setStiffnesses("Body", 1.0)
+    if(shouldRest):
+        motionProxy.rest()
     motionProxy.wakeUp()
 
     #turn the head, so its looking straight forward
     setHeadCentral()
 
     # Send robot to Stand Init
+
     motionProxy.moveInit()
 
 
@@ -191,14 +210,17 @@ def main(pip, pport):
     global visionProxy
     visionProxy = ALProxy("RobocupVision", pip, pport)
 
-    initMotion()
-    ttsProxy.say("Hello Masters.")
+    initMotion(False)
+    #ttsProxy.say("Hello Masters.")
 
     ttsProxy.say("Going to search for the orange square.")
-    searchColorAndTakeFoto("orange_object", ORANGE_DOWN, ORANGE_UP)
+    searchColorAndTakeFoto("orange_object.png", ORANGE_DOWN, ORANGE_UP)
 
     ttsProxy.say("Going to search for the pink square.")
-    searchColorAndTakeFoto("pink_object", PINK_DOWN, PINK_UP)
+    searchColorAndTakeFoto("pink_object.png", PINK_DOWN, PINK_UP)
+
+
+
 
     ttsProxy.say("Finished my job. Goodbye")
     motionProxy.rest()
